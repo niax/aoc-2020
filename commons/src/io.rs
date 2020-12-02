cd2fadd9fc5e35c030ccf16d6d518cea9a1851d7
Nicholas Hollett <niax@niax.co.uk>
2020-12-02 19:09:30 +0000

    Pull file loading into a commons module.

---
+++ commons/src/io.rs
@@ -0,0 +1,17 @@
+use std::fmt::Debug;
+use std::fs::File;
+use std::str::FromStr;
+use std::io::prelude::*;
+
+pub fn load_file_lines<T>(path: &str) -> Vec<T> 
+        where T: FromStr, <T as FromStr>::Err : Debug {
+    let mut file = File::open(path).expect("Could not open input file");
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("Could not read input file");
+    content.lines().map(|l| {
+        match T::from_str(l) {
+            Ok(i) => i,
+            Err(e) => panic!("Couldn't parse '{}' => {:?}", l, e),
+        }
+    }).collect()
+}
