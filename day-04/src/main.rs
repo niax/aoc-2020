828120b6ec35364ba3ccc4e1f33df81804ddccd0
Nicholas Hollett <niax@niax.co.uk>
2020-12-04 19:24:14 +0000

    Solve day 4

---
+++ day-04/src/main.rs
@@ -0,0 +1,185 @@
+use commons::io::{load_file, FromLines};
+use derive_builder::Builder;
+use lazy_static::lazy_static;
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+enum Distance {
+    Centimeters(u32),
+    Inches(u32),
+}
+
+#[derive(Debug)]
+enum DistanceFromStrErr {
+    InvalidUnit(String),
+}
+
+impl FromStr for Distance {
+    type Err = DistanceFromStrErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut size_str = String::new();
+        let mut unit = String::new();
+        for c in s.chars() {
+            if c.is_ascii_digit() {
+                size_str.push(c);
+            } else {
+                unit.push(c);
+            }
+        }
+
+        let size: u32 = size_str.parse().unwrap();
+        match unit.as_str() {
+            "cm" => Ok(Distance::Centimeters(size)),
+            "in" => Ok(Distance::Inches(size)),
+            _ => Err(DistanceFromStrErr::InvalidUnit(unit)),
+        }
+    }
+}
+
+#[derive(Debug, Builder)]
+struct Passport {
+    birth_year: u32,
+    issue_year: u32,
+    expiration_year: u32,
+    height: String,
+    hair_colour: String,
+    eye_colour: String,
+    passport_id: String,
+    #[builder(setter(into, strip_option), default)]
+    country_id: Option<String>,
+}
+
+impl Passport {
+    fn valid_birth_year(&self) -> bool {
+        self.birth_year >= 1920 && self.birth_year <= 2002
+    }
+
+    fn valid_issue_year(&self) -> bool {
+        self.issue_year >= 2010 && self.issue_year <= 2020
+    }
+
+    fn valid_expiration_year(&self) -> bool {
+        self.expiration_year >= 2020 && self.expiration_year <= 2030
+    }
+
+    fn valid_height(&self) -> bool {
+        match self.height.parse() {
+            Ok(Distance::Centimeters(cm)) => cm >= 150 && cm <= 193,
+            Ok(Distance::Inches(i)) => i >= 59 && i <= 76,
+            _ => false,
+        }
+    }
+
+    fn valid_hair_colour(&self) -> bool {
+        let mut chars = self.hair_colour.chars();
+        match chars.next().unwrap() {
+            '#' => chars.all(|c| c.is_ascii_hexdigit()),
+            _ => false,
+        }
+    }
+
+    fn valid_eye_colour(&self) -> bool {
+        lazy_static! {
+            static ref VALID_COLOURS: Vec<&'static str> =
+                vec!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+        }
+
+        VALID_COLOURS
+            .iter()
+            .any(|valid| valid == &self.eye_colour.as_str())
+    }
+
+    fn valid_passport_id(&self) -> bool {
+        self.passport_id.len() == 9 && self.passport_id.chars().all(|c| c.is_ascii_digit())
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.valid_birth_year()
+            && self.valid_issue_year()
+            && self.valid_expiration_year()
+            && self.valid_height()
+            && self.valid_hair_colour()
+            && self.valid_eye_colour()
+            && self.valid_passport_id()
+    }
+}
+
+#[derive(Debug)]
+struct PassportStore {
+    passports: Vec<Passport>,
+}
+
+impl FromLines for PassportStore {
+    type Line = String;
+
+    fn from_lines<I>(lines: &mut I) -> Self
+    where
+        I: Iterator<Item = Self::Line>,
+    {
+        let mut passports = Vec::new();
+        let mut current = PassportBuilder::default();
+        for line in lines {
+            if line.len() == 0 {
+                match current.build() {
+                    Ok(passport) => {
+                        passports.push(passport);
+                    }
+                    Err(_) => {}
+                }
+                current = PassportBuilder::default();
+                continue;
+            }
+            // Pull out the fields, put them in the current passport
+            line.split_whitespace().for_each(|f| {
+                let mut field_iter = f.split(":");
+                let key = field_iter.next().unwrap();
+                let value = field_iter.next().unwrap().to_string();
+                match key {
+                    "byr" => {
+                        current.birth_year(value.parse().unwrap());
+                    }
+                    "iyr" => {
+                        current.issue_year(value.parse().unwrap());
+                    }
+                    "eyr" => {
+                        current.expiration_year(value.parse().unwrap());
+                    }
+                    "hgt" => {
+                        current.height(value.parse().unwrap());
+                    }
+                    "hcl" => {
+                        current.hair_colour(value.to_string());
+                    }
+                    "ecl" => {
+                        current.eye_colour(value.to_string());
+                    }
+                    "pid" => {
+                        current.passport_id(value.to_string());
+                    }
+                    "cid" => {
+                        current.country_id(value.to_string());
+                    }
+                    _ => panic!(format!("I don't know this field: '{}'", key)),
+                }
+            });
+        }
+        match current.build() {
+            Ok(passport) => {
+                passports.push(passport);
+            }
+            Err(_) => {}
+        }
+
+        PassportStore { passports }
+    }
+}
+
+fn main() {
+    let store = load_file::<PassportStore>("input.txt");
+
+    println!("{}", store.passports.len());
+
+    let valid = store.passports.iter().filter(|p| p.is_valid()).count();
+    println!("{}", valid);
+}
