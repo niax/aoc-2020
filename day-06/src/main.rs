8a03262848fddc75ff6b23bc57898f71d25ebf68
Nicholas Hollett <niax@niax.co.uk>
2020-12-06 11:54:31 +0000

    Day 6 - Part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -3,7 +3,27 @@ use std::collections::HashSet;
 
 #[derive(Debug)]
 struct CustomsGroupStore {
-    groups: Vec<HashSet<char>>,
+    groups: Vec<CustomsGroup>,
+}
+
+#[derive(Debug)]
+struct CustomsGroup {
+    yes_answers: Vec<HashSet<char>>
+}
+
+impl CustomsGroup {
+    pub fn uniq_answers(&self) -> HashSet<char> {
+        self.yes_answers.iter().flat_map(|s| s).map(|c| *c).collect()
+    }
+
+    pub fn answer_intersection(&self) -> HashSet<char> {
+        let mut intersection = self.yes_answers[0].clone();
+        for answers in &self.yes_answers {
+            intersection = intersection.intersection(&answers).map(|c| *c).collect();
+        }
+
+        intersection
+    }
 }
 
 impl FromLines for CustomsGroupStore {
@@ -14,20 +34,26 @@ impl FromLines for CustomsGroupStore {
         I: Iterator<Item = Self::Line>,
     {
         let mut groups = Vec::new();
-        let mut current = HashSet::new();
+        let mut current_group = Vec::new();
 
         for line in lines {
             if line.len() == 0 {
-                groups.push(current);
-                current = HashSet::new();
+                groups.push(CustomsGroup {
+                    yes_answers: current_group,
+                });
+                current_group = Vec::new();
                 continue;
             }
 
+            let mut person = HashSet::new();
             for c in line.chars() {
-                current.insert(c);
+                person.insert(c);
             }
+            current_group.push(person);
         }
-        groups.push(current);
+        groups.push(CustomsGroup {
+            yes_answers: current_group,
+        });
 
         CustomsGroupStore {
             groups
@@ -39,8 +65,12 @@ fn main() {
     let store = load_file::<CustomsGroupStore>("input.txt");
 
     let yes_count = store.groups.iter()
-        .map(|yeses| yeses.len())
+        .map(|group| group.uniq_answers().len())
         .fold(0, |a, c| a + c);
     println!("{}", yes_count);
 
+    let part2 = store.groups.iter()
+        .map(|group| group.answer_intersection().len())
+        .fold(0, |a, c| a + c);
+    println!("{}", part2);
 }
