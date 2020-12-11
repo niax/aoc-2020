1fdf2c6ab906cc7b13deb0817aa2dbeaa06ee08e
Nicholas Hollett <niax@niax.co.uk>
2020-12-11 14:28:27 +0000

    Solve day 11 part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,4 +1,5 @@
 use commons::io::load_file_lines;
+use lazy_static::lazy_static;
 use std::fmt;
 
 #[derive(Copy, Clone, PartialEq, Eq)]
@@ -25,43 +26,89 @@ impl Position {
             Position::Occupied => '#',
         }
     }
+
+    pub fn is_seat(&self) -> bool {
+        match self {
+            Position::Floor => false,
+            Position::Empty | Position::Occupied => true,
+        }
+    }
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone)]
 struct SeatPlan {
     seats: Vec<Vec<Position>>,
+    width: usize,
+}
+
+lazy_static! {
+    static ref AROUND: Vec<(i32, i32)> = vec![
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (-1, 0),
+        (1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    ];
 }
 
 impl SeatPlan {
-    fn at(&self, x: usize, y: usize) -> Option<&Position> {
-        self.seats.get(y).map(|row| row.get(x)).flatten()
+    pub fn new(seats: Vec<Vec<Position>>) -> SeatPlan {
+        let width = seats[0].len();
+        SeatPlan { seats, width }
+    }
+
+    fn at(&self, x: i32, y: i32) -> Option<&Position> {
+        if x < 0 || y < 0 {
+            None
+        } else {
+            self.seats
+                .get(y as usize)
+                .map(|row| row.get(x as usize))
+                .flatten()
+        }
     }
 
     fn around(&self, x: usize, y: usize) -> Vec<&Position> {
-        vec![
-            (-1, -1),
-            (0, -1),
-            (1, -1),
-            (-1, 0),
-            (1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-        ]
-        .iter()
-        .map(|offset| {
-            let (x_off, y_off) = offset;
-            let new_x = x as i32 + x_off;
-            let new_y = y as i32 + y_off;
-            if new_x < 0 || new_y < 0 {
-                None
-            } else {
-                self.at(new_x as usize, new_y as usize)
-            }
-        })
-        .filter(|opt| opt.is_some())
-        .map(|opt| opt.unwrap())
-        .collect()
+        AROUND
+            .iter()
+            .map(|offset| {
+                let (x_off, y_off) = offset;
+                let new_x = x as i32 + x_off;
+                let new_y = y as i32 + y_off;
+                self.at(new_x, new_y)
+            })
+            .filter(|opt| opt.is_some())
+            .map(|opt| opt.unwrap())
+            .collect()
+    }
+
+    fn first_seen(&self, x: usize, y: usize) -> Vec<&Position> {
+        AROUND
+            .iter()
+            .map(|gradient| {
+                let (x_grad, y_grad) = gradient;
+                let mut new_x = x as i32 + x_grad;
+                let mut new_y = y as i32 + y_grad;
+                loop {
+                    let seat_opt = self.at(new_x, new_y);
+                    let seat = match seat_opt {
+                        Some(seat) => seat,
+                        None => return None, // We ran off the world
+                    };
+
+                    if seat.is_seat() {
+                        return Some(seat);
+                    }
+                    new_x += x_grad;
+                    new_y += y_grad;
+                }
+            })
+            .filter(|opt| opt.is_some())
+            .map(|opt| opt.unwrap())
+            .collect()
     }
 
     pub fn step(&self) -> SeatPlan {
@@ -93,7 +140,44 @@ impl SeatPlan {
             new_rows.push(new_row);
         }
 
-        SeatPlan { seats: new_rows }
+        SeatPlan::new(new_rows)
+    }
+
+    pub fn step_part2(&self) -> SeatPlan {
+        let new_rows = self
+            .seats
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(x, seat)| {
+                        let around = self.first_seen(x, y);
+                        let occupied_count =
+                            around.iter().filter(|s| ***s == Position::Occupied).count();
+                        match seat {
+                            Position::Empty => {
+                                if occupied_count > 0 {
+                                    Position::Empty
+                                } else {
+                                    Position::Occupied
+                                }
+                            }
+                            Position::Occupied => {
+                                if occupied_count >= 5 {
+                                    Position::Empty
+                                } else {
+                                    Position::Occupied
+                                }
+                            }
+                            Position::Floor => Position::Floor,
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        SeatPlan::new(new_rows)
     }
 }
 
@@ -110,32 +194,44 @@ impl fmt::Display for SeatPlan {
     }
 }
 
-fn main() {
-    let lines = load_file_lines::<String>("input.txt")
-        .map(|res| {
-            res.unwrap()
-                .chars()
-                .map(|c| Position::from_char(c).unwrap())
-                .collect()
-        })
-        .collect();
-    let mut plan = SeatPlan { seats: lines };
-
+fn converge<F>(in_plan: &SeatPlan, step: F) -> usize
+where
+    F: Fn(&SeatPlan) -> SeatPlan,
+{
+    let mut plan = in_plan.clone();
     loop {
-        let new_plan = plan.step();
+        println!("{}", plan);
+        let new_plan = step(&plan);
         if new_plan == plan {
             break;
         }
         plan = new_plan;
     }
-    let occupied: usize = plan
-        .seats
+    plan.seats
         .iter()
         .map(|row| {
             row.iter()
                 .filter(|seat| **seat == Position::Occupied)
                 .count()
         })
-        .sum();
-    println!("{}", occupied);
+        .sum()
+}
+
+fn main() {
+    let plan = SeatPlan::new(
+        load_file_lines::<String>("input.txt")
+            .map(|res| {
+                res.unwrap()
+                    .chars()
+                    .map(|c| Position::from_char(c).unwrap())
+                    .collect()
+            })
+            .collect(),
+    );
+
+    let part1 = converge(&plan, |p| p.step());
+    println!("{}", part1);
+
+    let part2 = converge(&plan, |p| p.step_part2());
+    println!("{}", part2);
 }
