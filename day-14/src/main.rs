af3fcecbca25838b9c836d1f9056662d61d66476
Nicholas Hollett <niax@niax.co.uk>
2020-12-14 13:46:28 +0000

    Solve day 14, part 2

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -6,7 +6,7 @@ use thiserror::Error;
 
 #[derive(Debug)]
 enum Action {
-    SetMask(u64, u64),
+    SetMask(u64, u64, u64),
     SetValue(usize, u64),
 }
 
@@ -31,6 +31,7 @@ impl FromStr for Action {
         if action == "mask" {
             let mut set_bits = 0;
             let mut clear_bits = 0;
+            let mut float_bits = 0;
             // Parse the mask
             for (i, bit) in rev(parts[2].chars()).enumerate() {
                 let bitmask = 1 << i;
@@ -41,11 +42,13 @@ impl FromStr for Action {
                     '0' => {
                         clear_bits |= bitmask;
                     }
-                    'X' => {}
+                    'X' => {
+                        float_bits |= bitmask;
+                    }
                     _ => return Err(ParseError::BadMask),
                 }
             }
-            Ok(Action::SetMask(set_bits, clear_bits))
+            Ok(Action::SetMask(set_bits, clear_bits, float_bits))
         } else if &action[0..4] == "mem[" {
             let cell_num_str: String = action
                 .chars()
@@ -61,25 +64,72 @@ impl FromStr for Action {
     }
 }
 
-fn main() {
-    let input =
-        load_file_lines::<Action>("input.txt").map(|res| res.expect("Failed to read input"));
-
+fn run_part1(input: &Vec<Action>) -> HashMap<usize, u64> {
     let mut set_bits = 0;
     let mut clear_bits = 0;
     let mut memory: HashMap<usize, u64> = HashMap::new();
     for i in input {
         match i {
-            Action::SetMask(set, clear) => {
-                set_bits = set;
-                clear_bits = clear
+            Action::SetMask(set, clear, _) => {
+                set_bits = *set;
+                clear_bits = *clear
             }
             Action::SetValue(cell, val) => {
-                let val = val | set_bits;
+                let val = *val | set_bits;
                 let val = val & !clear_bits;
-                memory.insert(cell, val);
+                memory.insert(*cell, val);
             }
         }
     }
-    println!("{}", memory.values().sum::<u64>());
+
+    memory
+}
+
+fn run_part2(input: &Vec<Action>) -> HashMap<usize, u64> {
+    let mut set_bits = 0;
+    let mut floating_bits = 0;
+    let mut memory: HashMap<usize, u64> = HashMap::new();
+    for i in input {
+        match i {
+            Action::SetMask(set, _, floating) => {
+                set_bits = *set as usize;
+                floating_bits = *floating as usize;
+            }
+            Action::SetValue(cell, val) => {
+                let cell = cell | set_bits;
+                let mut cells = Vec::new();
+                for i in 0..36 {
+                    let bit = floating_bits & (1 << i);
+                    if bit != 0 {
+                        let mut new_cells = cells.clone();
+                        for &previous_cell in &cells {
+                            new_cells.push(previous_cell | bit);
+                            new_cells.push(previous_cell & !bit);
+                        }
+                        // Add new values for just this bit
+                        new_cells.push(cell | bit);
+                        new_cells.push(cell & !bit);
+                        cells = new_cells;
+                    }
+                }
+                for cell in cells {
+                    memory.insert(cell, *val);
+                }
+            }
+        }
+    }
+
+    memory
+}
+
+fn main() {
+    let input = load_file_lines("input.txt")
+        .map(|res| res.expect("Failed to read input"))
+        .collect();
+
+    let part1 = run_part1(&input);
+    println!("{}", part1.values().sum::<u64>());
+
+    let part2 = run_part2(&input);
+    println!("{}", part2.values().sum::<u64>());
 }
