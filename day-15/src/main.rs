8ae7ebccc30863798379df1a6ba8a9a4c7b280c4
Nicholas Hollett <niax@niax.co.uk>
2020-12-16 00:41:23 +0000

    Refactor day 15 to use a dedicated wrapper

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,5 +1,6 @@
 use commons::io::load_file_lines;
 use std::collections::{HashMap, VecDeque};
+use std::hash::Hash;
 
 #[derive(Debug)]
 struct MostRecent<T> {
@@ -28,6 +29,36 @@ impl<T> MostRecent<T> {
     }
 }
 
+#[derive(Debug)]
+struct RecencyMap<K, V> {
+    map: HashMap<K, MostRecent<V>>,
+    n: usize,
+}
+
+impl <K, V> RecencyMap<K, V>
+where
+    K: Eq + Hash + Copy
+{
+    pub fn new(n: usize) -> RecencyMap<K, V> {
+        RecencyMap {
+            map: HashMap::new(),
+            n,
+        }
+    }
+
+    fn get_key(&mut self, key: &K) -> &mut MostRecent<V> {
+        self.map.entry(*key).or_insert(MostRecent::new(self.n))
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        self.get_key(&key).push(value)
+    }
+
+    pub fn get(&self, key: &K, nth: usize) -> Option<&V> {
+        self.map.get(key).map(|recent| recent.get(nth)).flatten()
+    }
+}
+
 fn main() {
     let lines: Vec<String> = load_file_lines::<String>("input.txt")
         .map(|res| res.unwrap())
@@ -37,26 +68,19 @@ fn main() {
         .map(|res| res.parse().unwrap())
         .collect();
 
-    let mut last_occurance: HashMap<u32, MostRecent<usize>> = HashMap::new();
+    let mut last_occurance: RecencyMap<u32, usize> = RecencyMap::new(2);
     let mut last_number = 0;
     for (i, val) in input.iter().enumerate() {
-        let idxs = last_occurance.entry(*val).or_insert(MostRecent::new(2));
-        idxs.push(i);
+        last_occurance.insert(*val, i);
         last_number = *val;
     }
 
     for i in input.len()..30000000 {
-        let next_number = match last_occurance.get(&last_number) {
-            Some(n) => match (n.get(0), n.get(1)) {
-                (Some(a), Some(b)) => (a - b) as u32,
-                _ => 0,
-            },
-            None => 0,
+        let next_number = match (last_occurance.get(&last_number, 0), last_occurance.get(&last_number, 1)) {
+            (Some(a), Some(b)) => (a - b) as u32,
+            _ => 0,
         };
-        let idxs = last_occurance
-            .entry(next_number)
-            .or_insert(MostRecent::new(2));
-        idxs.push(i);
+        last_occurance.insert(next_number, i);
         last_number = next_number;
         if i == 2019 {
             println!("{}", last_number);
