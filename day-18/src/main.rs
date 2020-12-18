243695a6a06b6c07261b48c8e1acc68c59087fe4
Nicholas Hollett <niax@niax.co.uk>
2020-12-18 19:07:35 +0000

    Solve day 17 part 2

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,13 +1,21 @@
 use commons::io::load_file_lines;
-use day_18::{grammar, types};
+use day_18::grammar;
 
 fn main() {
-    let parser = grammar::ExpressionParser::new();
-    let input: Vec<types::Expr> = load_file_lines::<String>("input.txt")
+    let ltr_parser = grammar::LtrExpressionParser::new();
+    let infix_parser = grammar::InfixExpressionParser::new();
+    let input: Vec<String> = load_file_lines::<String>("input.txt")
         .map(|res| res.unwrap())
-        .map(|s| parser.parse(s.as_str()).unwrap())
         .collect();
 
-    let part1: i64 = input.iter().map(|i| i.evaluate()).sum();
+    let part1: i64 = input
+        .iter()
+        .map(|i| ltr_parser.parse(i).unwrap().evaluate())
+        .sum();
     println!("{}", part1);
+    let part2: i64 = input
+        .iter()
+        .map(|i| infix_parser.parse(i).unwrap().evaluate())
+        .sum();
+    println!("{}", part2);
 }
